63e44311b3a79d923522d28ad216855cd15196a3
Rizwan Tariq <rizwantariq95@outlook.com>
2022-02-17 10:23:49 +0500

    Initialize store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    authShow: true,
   },
   mutations: {
+    toggleAuth: (state) => {
+      state.authShow = !state.authShow;
+      console.log(state.authShow);
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
