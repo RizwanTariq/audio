a504f41f61a28c93f89d732c9169dcc8d9b1715b
Rizwan Tariq <rizwantariq95@outlook.com>
2022-02-21 21:23:20 +0500

    registered validation plugin.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import veeValidatePlugin from './plugins/validation';
 import './assets/tailwind.css';
 import './assets/css/main.css';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).use(veeValidatePlugin).mount('#app');
