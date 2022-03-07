121d2e52770a74265a683fe5821005029590a8fd
Rizwan Tariq <rizwantariq95@outlook.com>
2022-03-07 15:35:46 +0500

    fixed Fade bug.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
   <!-- Header -->
   <app-header />
 
-  <router-view v-slot="{ Component }">
+  <router-view />
+  <!-- <router-view v-slot="{ Component }">
     <transition name="fade" mode="out-in">
       <component :is="Component"></component>
     </transition>
-  </router-view>
+  </router-view> -->
 
   <!-- Player -->
   <player />
